5486dd19c46fcd2b1758aa1b882823c6e1dd36f7
cao_boi_dia_nguc <jubivu@icloud.com>
2023-09-29 21:04:45 -0400

    fixed email button

---
+++ DayEarns/khach/AddEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  AddEmailView.swift
+//  DayEarns
+//
+//  Created by Jubi on 9/29/23.
+//
+
+import SwiftUI
+
+struct AddEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddEmailView()
+}
